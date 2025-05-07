3445e5004924bf5bea446049e5c937ad3f62c31e
Cauan Gomes <cauangomes.b@gmail.com>
2025-05-07 18:06:10 -0300

    add atualizações

--- kwaipie/Cenas/player_1.gd
+++ kwaipie/Cenas/player_1.gd
@@ -1,19 +1,19 @@
 extends CharacterBody2D
 
-signal player_clicked
+#signal player_clicked
 
 func _on_ready():
 	$AnimatedSprite2D.play("boy1")
-	var click_area = $ClickArea  
-	if click_area == null:
-		print("Erro: Não foi possível encontrar o nó ClickArea.")
-		return
+	#var click_area = $ClickArea  
+	#if click_area == null:
+	#	print("Erro: Não foi possível encontrar o nó ClickArea.")
+	#	return
 	
 	# Conecta o evento de clique
-	click_area.input_event.connect(_on_input_event)
+	#click_area.input_event.connect(_on_input_event)
    
 
-func _on_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+#func _on_input_event(viewport, event, shape_idx):
+	#if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		# Emite o sinal de clique quando o jogador é clicado
-		player_clicked.emit()
+	#	player_clicked.emit()
--- kwaipie/Cenas/player_2.gd
+++ kwaipie/Cenas/player_2.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
 
-signal player_clicked
+#signal player_clicked
 
 func _on_ready():
 	$AnimatedSprite2D.play("girl1")
 	
-func _ready():
-	$ClickArea.input_event.connect(_on_input_event)
+#func _ready():
+#	$ClickArea.input_event.connect(_on_input_event)
 
-func _on_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		player_clicked.emit()
+#func _on_input_event(viewport, event, shape_idx):
+#	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+#		player_clicked.emit()
