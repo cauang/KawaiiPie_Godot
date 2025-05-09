2c15a19e3afb2891ac49a38f3482a8adf82dd882
Cauan Gomes <cauangomes.b@gmail.com>
2025-05-09 16:47:07 -0300

    desisti de colocar o clickarea usar botão para selecionar o player

--- kwaipie/Cenas/player_1.gd
+++ kwaipie/Cenas/player_1.gd
@@ -1,26 +1,26 @@
+# player.gd
 extends CharacterBody2D
 
-@onready var click_area = $ClickArea  # Acessa o nó ClickArea
-@onready var animated_player = $AnimatedSprite2D  # Acessa o nó AnimatedSprite2D
-@export var player_scene_path: String = "res://Cenas/player_1.tscn"  # Caminho da cena do personagem
+@onready var click_area: Area2D = $ClickArea
+@onready var animated_player: AnimatedSprite2D = $AnimatedSprite2D
+@onready var indicator_label: Label = $PlayerIndicator
+
+@export var player_scene_path: String = "res://Cenas/player_1.tscn"
 
 signal player_clicked(player_node)
 
 func _ready():
-	# Verifica se o ClickArea existe e conecta o sinal de input_event corretamente
 	if click_area:
 		click_area.connect("input_event", Callable(self, "_on_click_area_input_event"))
 	else:
-		printerr("Erro: ClickArea não encontrado para o jogador.")
-	
-	# Toca a animação inicial
-	if animated_player:
-		animated_player.play("boy1")  # Substitua "boy1" se necessário
-	else:
-		printerr("Erro: AnimatedSprite2D não encontrado.")
+		printerr("ClickArea não encontrado no jogador: ", name)
 
-# Esta função será chamada quando o usuário clicar na ClickArea
-func _on_click_area_input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+	if animated_player:
+		animated_player.play("boy1")
+	else:
+		printerr("AnimatedSprite2D não encontrado")
+
+func _on_click_area_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print("Clique detectado em: ", name)
 		player_clicked.emit(self)
-		print("Jogador clicado: ", player_scene_path)
